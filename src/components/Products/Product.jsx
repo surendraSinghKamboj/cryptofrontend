cea5b6d3405dca4cc1a225c913245792352d8c35
kjassi560 <kjassi560@gmail.com>
2022-11-16 02:37:24 +0530

    Update Product.jsx
    Added Product page main div style

--- src/components/Products/Product.jsx
+++ src/components/Products/Product.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
 
 function Product() {
+const style={
+mainDiv:{
+background:"rgba(100,100,100,.8)",
+width:"100%",
+height:"90vh",
+textAlign:"center"
+}
+}
   return (
-    <div>Product</div>
+    <div style={style.mainDiv}>
+<h1>Product Page</h1>
+</div>
   )
 }
 
