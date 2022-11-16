fc7fd3d47bba7c40632a5f699143bdc700b7304e
Surendra Singh Kamboj <116861180+surendraSinghKamboj@users.noreply.github.com>
2022-11-16 14:56:35 +0530

    Update Exchange.jsx

--- src/components/Exchange/Exchange.jsx
+++ src/components/Exchange/Exchange.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 
 function Exchange() {
+const style={ mainDiv:{ width:"100%", height:"90vh", backgroundColor:"rgba(100,100,100,.2)", textAlign:"center", paddingTop:"20px" }}
   return (
-    <div>Exchange</div>
+    <div style={style.mainDiv}>
+<h1>Exchange</h1>
+</div>
   )
 }
 
