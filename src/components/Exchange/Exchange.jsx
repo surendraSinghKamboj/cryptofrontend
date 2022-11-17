dd3a31e7e74e2a1a4f42310f6f0dab5fa457f237
Surendra Singh Kamboj <116861180+surendraSinghKamboj@users.noreply.github.com>
2022-11-17 16:09:58 +0530

    some try prepared for api

--- src/components/Exchange/Exchange.jsx
+++ src/components/Exchange/Exchange.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
+import React from "react";
 
 function Exchange() {
-const style={ mainDiv:{ width:"100%", height:"90vh", backgroundColor:"rgba(100,100,100,.2)", textAlign:"center", paddingTop:"20px" }}
+  const style = {
+    mainDiv: {
+      minHeight: "90vh",
+      textAlign: "center",
+      paddingTop: "20px",
+      transition:"all .2s"
+    },
+  };
   return (
-    <div style={style.mainDiv}>
-<h1>Exchange</h1>
-</div>
-  )
+    <div style={style.mainDiv} className="glass">
+      <h1>Exchange</h1>
+    </div>
+  );
 }
 
-export default Exchange
+export default Exchange;
